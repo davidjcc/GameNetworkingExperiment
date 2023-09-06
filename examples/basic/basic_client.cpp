480f92bbd74bd29b056ed8a1dda1e60e95be858a
David Connett <davidcon90@gmail.com>
2023-09-06 20:04:58 +0100

    Added basic example and updated README.

---
+++ examples/basic/basic_client.cpp
@@ -0,0 +1,42 @@
+#include <bs/enet.h>
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+int main() {
+	auto logger = spdlog::stdout_color_mt("CLIENT");
+
+	ENet enet(logger);
+
+	Host_Client* client = enet.create_host_client();
+	client->start("127.0.0.1", 1234);
+
+	while (1) {
+		client->tick(0);
+
+		auto& packets = client->get_packets();
+		while (!packets.empty()) {
+			auto packet = packets.pop_front();
+			switch (packet.get_type()) {
+			case Packet::NONE: break;
+
+			case Packet::CONNECT: {
+				client->get_logger()->info("Connected to server");
+				break;
+			}
+
+			case Packet::DISCONNECT: {
+				client->get_logger()->info("Disconnected from serve");
+				break;
+			}
+
+			case Packet::EVENT_RECIEVED: {
+				client->get_logger()->info("Packet recieved");
+				break;
+			}
+			}
+		}
+	}
+
+	enet.destroy_client(client);
+	return 0;
+}
---
+++ examples/basic/basic_server.cpp
@@ -0,0 +1,42 @@
+#include <bs/enet.h>
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+int main() {
+	auto logger = spdlog::stdout_color_mt("SERVER");
+
+	ENet enet(logger);
+
+	Host_Server* server = enet.create_server("127.0.0.1", 1234, 1);
+	server->start();
+
+	while (1) {
+		server->tick(0);
+
+		auto& packets = server->get_packets();
+		while (!packets.empty()) {
+			auto packet = packets.pop_front();
+			switch (packet.get_type()) {
+			case Packet::NONE: break;
+
+			case Packet::CONNECT: {
+				server->get_logger()->info("Client connected");
+				break;
+			}
+
+			case Packet::DISCONNECT: {
+				server->get_logger()->info("Client disconnected");
+				break;
+			}
+
+			case Packet::EVENT_RECIEVED: {
+				server->get_logger()->info("Packet recieved");
+				break;
+			}
+			}
+		}
+	}
+
+	enet.destroy_server(server);
+	return 0;
+}
